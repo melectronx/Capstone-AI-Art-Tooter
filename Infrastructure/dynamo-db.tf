b904933fd391d090ec3af38550f82c6fe671698e
Martin Koeller <koeller2203@gmail.com>
2022-11-24 15:12:51 +0100

    changed suggestions from pull request

--- Infrastructure/dynamo-db.tf
+++ Infrastructure/dynamo-db.tf
@@ -1,15 +1,10 @@
 resource "aws_dynamodb_table" "toots" {
   name     = "toots"
   hash_key = "id"
-
+  read_capacity  = 5
+  write_capacity = 5
   attribute {
     name = "id"
     type = "S"
   }
-
-  read_capacity  = 5
-  write_capacity = 5
-
-  stream_enabled = true
-  stream_view_type = "NEW_AND_OLD_IMAGES"
 }
--- upload-src.sh
+++ upload-src.sh
@@ -1,9 +1,9 @@
 # without sandbox please move creation of the bucket to terraform
-sh setup_src_bucket.sh
-
+sh setup_bucket.sh terraform-state-ai-art-tooter 
+sh setup_bucket.sh ai-art-tooter-src-bucket
+sh setup_bucket.sh ai-art-tooter-img
 
 mkdir infrastructure/build
-
 cd get-toot/src
 zip -r ../../infrastructure/build/get-toot.zip .
 cd ../..
@@ -12,10 +12,10 @@ cd post-toot/src
 zip -r ../../infrastructure/build/post-toot.zip .
 cd ../..
 
-cd requests-layer
+cd mastodon-layer
 pip3 install -r requirements.txt --target python/lib/python3.9/site-packages
-zip -r ../build/requests-layer.zip .
+zip -r ../build/mastodon-layer.zip .
 cd ..
 
 echo  "upload to s3"
-aws s3 cp build/requests-layer.zip s3://ai-art-tooter-src-bucket/
+aws s3 cp build/mastodon-layer.zip s3://ai-art-tooter-src-bucket/

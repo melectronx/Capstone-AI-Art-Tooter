4f024a933a8225437bb66319d8c2aefa36e19aba
Martin Koeller <koeller2203@gmail.com>
2022-11-30 12:01:06 +0100

    update get-toot.py

--- infrastructure/dynamo-db.tf
+++ infrastructure/dynamo-db.tf
@@ -1,15 +1,15 @@
 resource "aws_dynamodb_table" "toots" {
   name     = "toots"
   hash_key = "id"
-  range_key = "date"
+  #range_key = "date"
   read_capacity  = 5
   write_capacity = 5
   attribute {
     name = "id"
     type = "N"
   }
-  attribute {
-    name = "date"
-    type = "S"
-  }
+  #attribute {
+  #  name = "date"
+  #  type = "S"
+  #}
 }
--- source/get-toot/src/get-toot.py
+++ source/get-toot/src/get-toot.py
@@ -1,1 +1,36 @@
-code="in_dev"
+from mastodon import Mastodon, CallbackStreamListener
+import json
+import os
+import boto3
+from datetime import datetime
+
+mastodon = Mastodon(
+    access_token = 'pytooter_usercred.secret',
+    api_base_url = 'https://techhub.social'
+)
+dynamodb = boto3.resource('dynamodb')
+toots_table_name = "toots"     #os.getenv('TOOTS_TABLE_NAME')
+toots_table = dynamodb.Table(toots_table_name)
+
+tag = "aiarttooter"
+
+def save_toot(toot_data):
+    content = toot_data['content']
+    prompt = content[content.find('prompt')+7:-4]
+    toot = {
+            "id": toot_data["id"],
+            #"date": toot_data["created_at"],
+            "prompt": prompt,
+            "username": toot_data["account"]["username"],
+            "filename": str(toot_data['id'])+'.jpeg'
+        }
+        
+    toots_table.put_item(Item = toot)
+    
+    print(toot)
+
+
+
+if __name__ == "__main__":      
+    listener = CallbackStreamListener(save_toot)    
+    mastodon.stream_hashtag(tag, listener, local=False, run_async=False, timeout=300, reconnect_async=False, reconnect_async_wait_sec=5)

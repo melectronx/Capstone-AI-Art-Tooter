8f45b8fffd5517092a9baa073816ed002dec3fb2
Martin Koeller <koeller2203@gmail.com>
2022-12-01 17:15:12 +0100

    correct dynamodb.tf

--- source/get-toot/src/get-toot.py
+++ source/get-toot/src/get-toot.py
@@ -15,19 +15,20 @@ toots_table = dynamodb.Table(toots_table_name)
 tag = "aiarttooter"
 
 def save_toot(toot_data):
-    content = toot_data['content']
-    tootdate = toot_data['created_at']
-    datetoot = tootdate.strftime("%m/%d/%Y, %H:%M:%S")
-    prompt = content[content.find('prompt')+7:-4]
-    toot = {
-            "id": toot_data["id"],
-            "date": datetoot,
-            "prompt": prompt,
-            "username": toot_data["account"]["username"],
-            "filename": str(toot_data['id'])+'.jpeg'
-        }
-        
-    toots_table.put_item(Item = toot)
+    if toot_data['account']['username'] != 'melectronx_bot':
+        content = toot_data['content']
+        tootdate = toot_data['created_at']
+        datetoot = tootdate.strftime("%m/%d/%Y, %H:%M:%S")
+        prompt = content[content.find('prompt')+7:-4]
+        toot = {
+                "id": toot_data["id"],
+                "date": datetoot,
+                "prompt": prompt,
+                "username": toot_data["account"]["username"],
+                "filename": str(toot_data['id'])+'.jpeg'
+            }
+            
+        toots_table.put_item(Item = toot)
     
 def handler(event, context):
     listener = CallbackStreamListener(save_toot)    

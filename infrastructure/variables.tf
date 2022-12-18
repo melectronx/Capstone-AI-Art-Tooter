21d7b990710e95cfc8957ff0d45e02b4ca476455
Martin Koeller <koeller2203@gmail.com>
2022-12-18 22:51:43 +0100

    add variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,3 +2,21 @@ locals {
   account_id = data.aws_caller_identity.current.account_id
   iam_role   = join(":", ["arn:aws:iam:", local.account_id, "role/LabRole"])
 }
+
+variable "MASTODON_ACCESS_TOKEN" {
+  description = "Access Token for Mastodon"
+  type        = string
+  sensitive   = true
+}
+
+variable "STABILITY_HOST" {
+  description = "stability AI Host"
+  type        = string
+  sensitive   = true
+}
+
+variable "STABILITY_KEY" {
+  description = "stability AI key"
+  type        = string
+  sensitive   = true
+}
